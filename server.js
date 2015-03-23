aa5b2198daee67d75a1e5f9552ccb4728d4dd89c
superpoulet1337 <kevin.jully@free.fr>
2015-03-23 14:27:02 +0100

    server

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 function randomColor()
 {
-	return {"r": Math.floor((Math.random() * 255) + 1), "g": Math.floor((Math.random() * 255) + 1), "b": Math.floor((Math.random() * 255) + 1)};
+	return {"r": Math.floor((Math.random() * 255) + 1),
+	"g": Math.floor((Math.random() * 255) + 1),
+	"b": Math.floor((Math.random() * 255) + 1),
+	"a": Math.floor((Math.random()))};
 }
-
-var http = require('http'),
-	fs = require('fs');
-
-var server = http.createServer(function(req, res)
-	{
-		res.writeHead(200);
-		res.end('cc');
-	});
 
 var id = 0;
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(1337);
 
 io.sockets.on('connection', function(socket)
 	{
 		socket.color = randomColor();
 		socket.id = id++;
 		console.log(socket.id + " user");
-		socket.emit('message', 'Coucou fdp');
+
 		socket.emit('connected', {"id": socket.id, "color": socket.color});
 
 		socket.on('draw', function(draw)
@@ -32,5 +26,3 @@ io.sockets.on('connection', function(socket)
 					'drawing': draw});
 			});
 	});
-
-server.listen(1337);
