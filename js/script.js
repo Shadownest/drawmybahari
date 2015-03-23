211673dbd8429290409ad8712864f62eabf7d8dd
marakabaas <marakabaas@free.fr>
2015-03-23 12:01:03 +0100

    update index et js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@
 var c = document.getElementById("mon_canvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();      // Début du chemin
-ctx.moveTo(50,50);    // Le tracé part du point 50,50
-ctx.lineTo(200,200);  // Un segment est ajouté vers 200,200
-ctx.moveTo(200,50);   // Puis on saute jusqu'à 200,50
-ctx.lineTo(50,200);   // Puis on trace jusqu'à 50,200
+ctx.moveTo(400,200);    // Le tracé part du point 50,50
+ctx.lineTo(400,600);  // Un segment est ajouté vers 200,200
+ctx.moveTo(200,400);   // Puis on saute jusqu'à 200,50
+ctx.lineTo(600,400);   // Puis on trace jusqu'à 50,200
 ctx.closePath();      // Fermeture du chemin (facultative)
+ctx.fillStyle = "orange"; // Définition de la couleur de remplissage
+ctx.fill();
+ctx.strokeStyle = "black";
+ctx.stroke();
+
+ctx.beginPath();      // Début du chemin
+ctx.moveTo(400,200);    // Le tracé part du point 50,50
+ctx.lineTo(200,400);  // Un segment est ajouté vers 200,200
+ctx.moveTo(200,400);   // Puis on saute jusqu'à 200,50
+ctx.lineTo(600,400);   // Puis on trace jusqu'à 50,200
+ctx.closePath();      // Fermeture du chemin (facultative)
+ctx.fillStyle = "orange"; // Définition de la couleur de remplissage
+ctx.fill();
+ctx.strokeStyle = "black";
+ctx.stroke();
+
