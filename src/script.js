84e5a50547ef114655b5b2c768f13fdf59ec6598
Pascal <pascal.charreix@gmail.com>
2015-03-23 15:10:28 +0100

    maj canvas pour nodejs + trigger

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ function changeSize(size)
 		$('.circle').css('height', size);
 		$('.circle').css('width', size);
 		$('.circle').css('margin', (80 - size) / 2);
+		$('canvas').trigger('size', size);
 	}
 }
 function changeColor(source)
@@ -13,10 +14,18 @@ function changeColor(source)
 	var rgba = color.r+','+color.g+','+color.b+','+color.a;
 	$('#color').css('background-color', 'rgba('+rgba+')');
 	$('.circle').css('background-color', 'rgba('+rgba+')');
+	$('canvas').trigger('color', rgba);
 }
-function draw(x1, x2, y1, y2, size, color)
+function draw(x1, x2, y1, y2, size, rgba)
 {
-	
+	ctx.strokeStyle = 'rgba('+rgba+')';
+	ctx.fillStyle = 'rgba('+rgba+')';
+	ctx.lineWidth = size;
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+	ctx.closePath();
 }
 $('document').ready(function()
 {
@@ -31,4 +40,9 @@ $('document').ready(function()
 	$('.menu').css('height', height - 60);
 	$('#size').on('click', function(){changeSize(parseInt(prompt("Size ? (between 1 and 80)")));});
 	changeSize(10);
+	$('canvas').on('draw', function(options)
+	{
+		var rgba = options['r']+','+options['g']+','+options['b']+','+options['a'];
+		draw(options['x1'], options['x2'], options['y1'], options['y2'], options['size'], rgba);
+	});
 });
