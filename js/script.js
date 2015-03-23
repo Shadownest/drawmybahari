44473ede141d17c479fc29df7d6ddeb55f748800
marakabaas <marakabaas@free.fr>
2015-03-23 12:33:49 +0100

    maj marc

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ ctx.stroke();
 ctx.beginPath();      // Début du chemin
 ctx.moveTo(400,200);    // Le tracé part du point 50,50
 ctx.lineTo(200,400);  // Un segment est ajouté vers 200,200
-ctx.moveTo(200,400);   // Puis on saute jusqu'à 200,50
+ctx.moveTo(400,200);   // Puis on saute jusqu'à 200,50
 ctx.lineTo(600,400);   // Puis on trace jusqu'à 50,200
 ctx.closePath();      // Fermeture du chemin (facultative)
 ctx.fillStyle = "orange"; // Définition de la couleur de remplissage
